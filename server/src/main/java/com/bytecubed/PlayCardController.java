db5454b87de80d15a45b0ce37302d5e358523612
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2018-12-25 17:39:49 -0500

    @carlyledavis:  Updating controller to return latest card that was imported.

--- server/src/main/java/com/bytecubed/PlayCardController.java
+++ server/src/main/java/com/bytecubed/PlayCardController.java
@@ -3,12 +3,15 @@ package com.bytecubed;
 import com.bytecubed.models.Placement;
 import com.bytecubed.parser.RavensPowerPointParser;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -20,15 +23,27 @@ public class PlayCardController {
     public HttpEntity getPlayCard(){
         return ok("Test");
     }
+    private List<Placement> placements;
+    private Logger logger = LoggerFactory.getLogger(PlayCardController.class);
+
+    public PlayCardController(){
+        placements = new ArrayList<>();
+    }
 
     @PostMapping()
     public HttpEntity<List<Placement>> importCard(@RequestParam("file") MultipartFile file,
                                                   RedirectAttributes redirectAttributes) throws IOException {
         XMLSlideShow ppt = new XMLSlideShow(file.getInputStream());
-        System.out.println( "PowerPoint with slidecount:  "+ ppt.getSlides().size());
+        logger.debug( "PowerPoint with slidecount:  "+ ppt.getSlides().size());
 
-        return ok(new RavensPowerPointParser(ppt).extractPlayerPlacements());
+        this.placements = new RavensPowerPointParser(ppt).extractPlayerPlacements();
+
+        return ok(placements);
     }
 
+    @GetMapping()
+    public HttpEntity<List<Placement>> getPlayCards(){
+        return ok(placements);
+    }
 
 }
