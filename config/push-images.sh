f9a9ceaebdae83af4c72cfc76fb4fd61b909474a
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-01-07 10:11:03 -0500

    @carlyledavis:  tagging images for aws.

--- config/push-images.sh
+++ config/push-images.sh
@@ -1,6 +1,11 @@
 eval "$(aws ecr get-login --no-include-email --region us-east-1)"
 DOCKER_REPO_URL=068681799287.dkr.ecr.us-east-1.amazonaws.com
 
+docker tag bytecubedlabs/playcards-client ${DOCKER_REPO_URL}/playcards-client
+docker tag bytecubedlabs/playcards-server ${DOCKER_REPO_URL}/playcards-server
+docker tag bytecubedlabs/playcards-nlp ${DOCKER_REPO_URL}/playcards-nlp
+docker tag bytecubedlabs/playcards-teams ${DOCKER_REPO_URL}/playcards-teams
+
 docker push ${DOCKER_REPO_URL}/playcards-client
 docker push ${DOCKER_REPO_URL}/playcards-server
 docker push ${DOCKER_REPO_URL}/playcards-nlp
