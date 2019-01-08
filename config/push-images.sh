7aa4819dcd9315863e2dfd201a6c5e14502bceb3
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-01-08 17:49:24 -0500

    @carlyledavis:  Converting to using the commit hash rather than build number since circle does interesting incrementing.

--- config/push-images.sh
+++ config/push-images.sh
@@ -1,12 +1,12 @@
 eval "$(aws ecr get-login --no-include-email --region us-east-1)"
 DOCKER_REPO_URL=068681799287.dkr.ecr.us-east-1.amazonaws.com
 
-docker tag bytecubedlabs/playcards-client ${DOCKER_REPO_URL}/playcards-client:${CIRCLE_BUILD_NUM}
-docker tag bytecubedlabs/playcards-server ${DOCKER_REPO_URL}/playcards-server:${CIRCLE_BUILD_NUM}
-docker tag bytecubedlabs/playcards-nlp ${DOCKER_REPO_URL}/playcards-nlp:${CIRCLE_BUILD_NUM}
-docker tag bytecubedlabs/playcards-teams ${DOCKER_REPO_URL}/playcards-teams:${CIRCLE_BUILD_NUM}
+docker tag bytecubedlabs/playcards-client ${DOCKER_REPO_URL}/playcards-client:${CIRCLE_SHA1}
+docker tag bytecubedlabs/playcards-server ${DOCKER_REPO_URL}/playcards-server:${CIRCLE_SHA1}
+docker tag bytecubedlabs/playcards-nlp ${DOCKER_REPO_URL}/playcards-nlp:${CIRCLE_SHA1}
+docker tag bytecubedlabs/playcards-teams ${DOCKER_REPO_URL}/playcards-teams:${CIRCLE_SHA1}
 
-docker push ${DOCKER_REPO_URL}/playcards-client:${CIRCLE_BUILD_NUM}
-docker push ${DOCKER_REPO_URL}/playcards-server:${CIRCLE_BUILD_NUM}
-docker push ${DOCKER_REPO_URL}/playcards-nlp:${CIRCLE_BUILD_NUM}
-docker push ${DOCKER_REPO_URL}/playcards-teams:${CIRCLE_BUILD_NUM}
+docker push ${DOCKER_REPO_URL}/playcards-client:${CIRCLE_SHA1}
+docker push ${DOCKER_REPO_URL}/playcards-server:${CIRCLE_SHA1}
+docker push ${DOCKER_REPO_URL}/playcards-nlp:${CIRCLE_SHA1}
+docker push ${DOCKER_REPO_URL}/playcards-teams:${CIRCLE_SHA1}
--- config/tag-push.sh
+++ config/tag-push.sh
@@ -1,8 +1,8 @@
-docker tag bytecubedlabs/playcards-nlp:latest bytecubedlabs/playcards-nlp:$CIRCLE_BUILD_NUM
-docker tag bytecubedlabs/playcards-client:latest bytecubedlabs/playcards-client:$CIRCLE_BUILD_NUM
-docker tag bytecubedlabs/playcards-server:latest bytecubedlabs/playcards-server:$CIRCLE_BUILD_NUM
-docker tag bytecubedlabs/playcards-teams:latest bytecubedlabs/playcards-teams:$CIRCLE_BUILD_NUM
-docker push bytecubedlabs/playcards-teams:$CIRCLE_BUILD_NUM
-docker push bytecubedlabs/playcards-server:$CIRCLE_BUILD_NUM
-docker push bytecubedlabs/playcards-client:$CIRCLE_BUILD_NUM
-docker push bytecubedlabs/playcards-nlp:$CIRCLE_BUILD_NUM
+docker tag bytecubedlabs/playcards-nlp:latest bytecubedlabs/playcards-nlp:$CIRCLE_SHA1
+docker tag bytecubedlabs/playcards-client:latest bytecubedlabs/playcards-client:$CIRCLE_SHA1
+docker tag bytecubedlabs/playcards-server:latest bytecubedlabs/playcards-server:$CIRCLE_SHA1
+docker tag bytecubedlabs/playcards-teams:latest bytecubedlabs/playcards-teams:$CIRCLE_SHA1
+docker push bytecubedlabs/playcards-teams:$CIRCLE_SHA1
+docker push bytecubedlabs/playcards-server:$CIRCLE_SHA1
+docker push bytecubedlabs/playcards-client:$CIRCLE_SHA1
+docker push bytecubedlabs/playcards-nlp:$CIRCLE_SHA1
