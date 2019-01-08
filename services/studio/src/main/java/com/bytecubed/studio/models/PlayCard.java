9ec1e8fd977ea65e5b593d9b9930aec06c270b18
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-01-08 14:26:02 -0500

    @carlyledavis:  Removing missing property from bean.

--- services/studio/src/main/java/com/bytecubed/studio/models/PlayCard.java
+++ services/studio/src/main/java/com/bytecubed/studio/models/PlayCard.java
@@ -6,11 +6,25 @@ import java.util.List;
 import java.util.UUID;
 
 public class PlayCard {
-    public PlayCard(UUID id, List<PlayerMarker> extractPlayerMarkerPlacements) {
 
+    private final UUID id;
+    private final List<PlayerMarker> playerMarkers;
+    private UUID teamId;
+
+    public PlayCard(UUID id, List<PlayerMarker> playerMarkers) {
+        this.id = id;
+        this.playerMarkers = playerMarkers;
     }
 
     public List<PlayerMarker> getPlayers() {
         return null;
+    }
+
+    public UUID getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(UUID teamId) {
+        this.teamId = teamId;
     }
 }
