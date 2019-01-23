5a4b8126406941e32bec58068c6594342a55ff66
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-01-23 16:26:22 -0500

    @carlyledavis:  Adding servers for vpn access.

--- config/modules/routes/main.tf
+++ config/modules/routes/main.tf
@@ -5,3 +5,19 @@ resource "aws_route53_record" "records" {
   ttl     = "300"
   records = ["40.117.45.119"]
 }
+
+resource "aws_route53_record" "records" {
+  zone_id = "${var.zone_id}"
+  name    = "zero.immersivesports.ai"
+  type    = "A"
+  ttl     = "300"
+  records = ["40.113.201.39"]
+}
+
+resource "aws_route53_record" "records" {
+  zone_id = "${var.zone_id}"
+  name    = "zeros.immersivesports.ai"
+  type    = "A"
+  ttl     = "300"
+  records = ["40.113.201.39"]
+}
