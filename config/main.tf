158dbfc1a7494d2ffc31de891e4bb43192e6ad33
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-03-15 18:18:47 -0400

    @carlyledavis:  Updating with new aws background.

--- config/main.tf
+++ config/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "immersivesports-deployments"
+    bucket = "bytecubedlabs-deployments"
     key    = "deploynents/playcards"
     region = "us-east-1"
   }
