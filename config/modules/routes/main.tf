cad9c5899724df9e53c2899dd2170d29fba29a34
Carlyle S. Davis <carlyle.davis@bytecubed.com>
2019-03-22 12:26:52 -0400

    @carlyledavis:  Added configuration for additional routes for michigan.

--- config/modules/routes/main.tf
+++ config/modules/routes/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "records" {
-  zone_id = "${var.zone_id}"
+  zone_id = "${aws_route53_zone.immersive.zone_id}"
   name    = "michigan.immersivesports.ai"
   type    = "A"
   ttl     = "300"
@@ -46,6 +46,6 @@ resource "aws_route53_record" "demo_backend" {
   records = ["54.209.115.220"]
 }
 
-# resource "aws_route53_zone" "primary" {
-#   name = "platform.bytecubedlabs.co"
-# }
+resource "aws_route53_zone" "immersive" {
+   name = "immersivesports.ai"
+}
